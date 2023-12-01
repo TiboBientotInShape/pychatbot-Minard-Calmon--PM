fc7dec8b82c8aec8885029c3805151da40d99d02
TiboBientotInShape <thibaut.minard@efrei.net>
2023-12-01 17:03:58 +0100

    add file extension to gitignore

--- function.py
+++ function.py
@@ -1,8 +1,34 @@
 import os
-def list_of_files(directory, extension):
+def list_pres(directory, extension):
 
     files_names = []
     for filename in os.listdir(directory):
         if filename.endswith(extension):
-            files_names.append(filename)
+            res = filename.replace("Nomination_", "").replace(".txt", "").replace("1", "").replace("2", "")
+            if res not in files_names:
+                files_names.append(res)
     return files_names
+
+def pren_pres(list):
+    files_surnames = []
+    for i in list:
+        if i == 'Chirac':
+            files_surnames.append("Jack Chirac")
+
+        if i == 'Giscard dEstaing':
+            files_surnames.append("Valery Giscard dEstaing")
+
+        if i == 'Hollande':
+            files_surnames.append("François Hollande")
+
+        if i == 'Macron':
+            files_surnames.append("Emmanuel Macron")
+
+        if i == 'Mitterrand':
+            files_surnames.append("François Mitterrand")
+
+        if i == 'Sarkozy':
+            files_surnames.append("Nicolas Sarkozy")
+    return files_surnames
+
+
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from function import *
 if __name__ == '__main__':
     directory = "./Speeches"
-    files_name = list_of_files(directory, ".txt")
+    files_name = list_pres(directory, ".txt")
     print(files_name)
+    files_surnames = pren_pres(files_name)
+    print(files_surnames)
+
+
