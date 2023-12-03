389de044fbffdff0c3845c9bf4fa65692b4ffb63
TiboBientotInShape <thibaut.minard@efrei.net>
2023-12-03 22:12:30 +0100

    add file extension to gitignore

--- function.py
+++ function.py
@@ -1,4 +1,5 @@
 import os
+
 def list_pres(directory, extension):
 
     files_names = []
@@ -31,4 +32,45 @@ def pren_pres(list):
             files_surnames.append("Nicolas Sarkozy")
     return files_surnames
 
+def mots_nimport(L2D):
+    list_mots = []
+    verif = 0
+    longeur_list = len(L2D)
+    longueur_list_TF_IDF = len(L2D[0])
+    for mots in range(0,longeur_list):
+        for TF_IDF in range(1,longueur_list_TF_IDF):
+            if L2D[mots][TF_IDF]==0.0:
+                verif+=1
+        if verif==7:
+            list_mots.append(L2D[mots][0])
+        verif = 0
+    return(list_mots)
 
+def mots_import(L2D):
+    list_mots = []
+    somme = 0
+    longeur_list = len(L2D)
+    longueur_list_TF_IDF = len(L2D[0])
+    for mots in range(0,longeur_list):
+        for TF_IDF in range(1,longueur_list_TF_IDF):
+                somme+=L2D[mots][TF_IDF]
+        if somme>=50:
+            list_mots.append(L2D[mots][0])
+        somme = 0
+    return(list_mots)
+
+def Rep_words(nom):
+    text = ""
+    nom = nom.lower()
+    list_word = []
+    for filename in os.listdir(r'C:\Users\minar\PycharmProjects\pychatbot-Minard-Calmon--PM/cleaned'):
+        if nom in filename.lower():
+            with open(r'C:\Users\minar\PycharmProjects\pychatbot-Minard-Calmon--PM\cleaned/{file}'.format(file=filename),"r") as file:
+                text+=file.read()
+
+    text_TF = TF(text)
+
+    for word in text_TF:
+        if text_TF[word]>15:
+            list_word.append(word)
+    return(list_word)
--- main.py
+++ main.py
@@ -6,4 +6,11 @@ if __name__ == '__main__':
     files_surnames = pren_pres(files_name)
     print(files_surnames)
 
+list=TF_IDF(r'C:\Users\minar\PycharmProjects\pychatbot-Minard-Calmon--PM/cleaned')
+print(mots_nimport(list))
+
+list=TF_IDF(r'C:\Users\minar\PycharmProjects\pychatbot-Minard-Calmon--PM/cleaned')
+print(mots_import(list))
+
+print(Rep_words("Chirac"))
 
